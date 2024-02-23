27538ff222e748b9bd426595b9cb977e693c5660
ncgears <1918ncgears@gmail.com>
2024-02-23 12:59:26 -0500

    Adjust red and green to be muted

--- src/main/java/frc/team1918/robot/Constants.java
+++ src/main/java/frc/team1918/robot/Constants.java
@@ -568,8 +568,8 @@ public class Constants {
         public class Colors {
             public static final String NCGREEN = "#00B50F"; //approximate
             public static final String NCBLUE = "#0077B5";
-            public static final String RED = "#FF0000";
-            public static final String GREEN = "#00FF00";
+            public static final String RED = "#F44336";
+            public static final String GREEN = "#4CAF50";
             public static final String BLUE = "#0000FF";
             public static final String ORANGE = "#FFA500";
             public static final String BLACK = "#000000";
