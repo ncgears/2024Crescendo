43e90ee225dbf2e3d4bc6812ad6bdf7b31b2844c
ncgears8 <ncgears8@ncgears.com>
2024-03-12 20:39:12 -0400

    auton tuning

--- src/main/java/frc/team1918/robot/Constants.java
+++ src/main/java/frc/team1918/robot/Constants.java
@@ -138,7 +138,7 @@ public class Constants {
      */
     public static final class Auton {
         public static final boolean isDisabled = false; //Disable autonomous
-        public static final boolean kUseTracking = true; //enable target tracking during auton pathing
+        public static final boolean kUseTracking = false; //enable target tracking during auton pathing
         public static final double kMaxSpeedMetersPerSecond = 0.1;
         public static final double kMaxAccelMetersPerSecondSquared = 0.1;
         public static final double kMaxOmega = (kMaxSpeedMetersPerSecond / Math.hypot(0.584 / 2.0, 0.66 / 2.0));
